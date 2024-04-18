e2663fec942f55fd2f879628584053a7bad73e54
Thiago Ribeiro <trl.debrito@controledois.com.br>
2024-04-18 19:18:29 -0400

    ✨ Form Category
    Ajustes de validação e cadastro de categorias

--- src/app/modules/admin/pages/category/category-form/category-form.component.ts
+++ src/app/modules/admin/pages/category/category-form/category-form.component.ts
@@ -7,6 +7,7 @@ import { CategoryService } from 'src/app/shared/services/category.service';
 import { NotificationService } from 'src/app/shared/services/notification.service';
 import { SearchLoadingUnique } from 'src/app/shared/widget/search-loading-unique/search-loading-unique.interface';
 import { LoadingFull } from 'src/app/shared/interfaces/loadingFull.interface';
+import { DialogMessageService } from 'src/app/shared/services/dialog-message.service';
 
 @Component({
   selector: 'app-category-form',
@@ -20,6 +21,7 @@ export class CategoryFormComponent implements OnInit {
     message: 'Aguarde, carregando...'
   }
   public myForm: FormGroup = new FormGroup({
+    company_id: new FormControl(''),
     category_id: new FormControl(''),
     role: new FormControl(0, Validators.required),
     name: new FormControl('', Validators.required),
@@ -42,16 +44,25 @@ export class CategoryFormComponent implements OnInit {
         value: 0
       }
     ],
-    validation: false
+    validation: true
   };
+
+  public validationFields: Array<any> = [
+    { name: 'name', validation: true, msg: 'É necessário informar o nome'},
+  ];
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private categoryService: CategoryService,
     private notificationService: NotificationService,
+    private dialogMessageService: DialogMessageService,
     private router: Router,
   ) {
     this.formId = this.activatedRoute.snapshot.params['id'];
+  }
+
+  validateForm(): void {
+    this.validationFields.find((v) => v.name === 'name').validation = !!this.myForm.value.name;
   }
 
   ngOnInit(): void {
@@ -75,27 +86,55 @@ export class CategoryFormComponent implements OnInit {
     if (value) {
       this.myForm.patchValue(value);
 
+      this.getRole();
       this.searchCategory.searchFieldOn = value?.category;
       this.searchCategory.searchField.setValue(value?.category?.name);
     }
   }
 
   save(): void {
-    this.loadingFull.active = true;
+    this.validateForm();
 
-    this.myForm.value.category_id = this.searchCategory?.searchFieldOn?.id;
+    if (this.myForm.valid) {
+      this.loadingFull.active = true;
+      this.myForm.value.category_id = this.searchCategory?.searchFieldOn?.id || null;
 
-    this.categoryService.save(this.formId, this.myForm.value).pipe(
-      finalize(() => this.loadingFull.active = false),
-      catchError((error) => {
-        this.notificationService.warn(error.error);
-        return throwError(error);
-      }),
-      map(() => {
-        this.notificationService.success('Salvo com sucesso.');
-        this.router.navigate(['category']);
-      })
-    ).subscribe();
+      this.categoryService.save(this.formId, this.myForm.value).pipe(
+        finalize(() => this.loadingFull.active = false),
+        catchError((res) => {
+          let title = 'Atenção';
+            let message = 'Ocorreu um erro ao realizar o cadastro, tente novamente mais tarde.';
+            let message_next = '';
+
+            if (res?.error?.errors?.[0]?.field === 'name') {
+              title = 'Campo Nome';
+              message = res.error.errors[0].message;
+              message_next = 'É essecial que o nome da categoria seja informado. Sempre valide se a categoria que você está cadastrando já não está em nossa base de dados.';
+            }
+
+            this.dialogMessageService.openDialog({
+              icon: 'pan_tool',
+              iconColor: '#ff5959',
+              title: title,
+              message: message,
+              message_next: message_next,
+            });
+          return throwError(res);
+        }),
+        map(() => {
+          this.notificationService.success('Salvo com sucesso.');
+          this.router.navigate(['category']);
+        })
+      ).subscribe();
+    } else {
+      this.dialogMessageService.openDialog({
+        icon: 'priority_high',
+        iconColor: '#ff5959',
+        title: 'Campos inválidos',
+        message: 'Existem campos inválidos, verifique se todos os campos estão preenchidos corretamente.',
+        message_next: 'Todos os campos são obrigatórios, verifique se todos os campos estão preenchidos corretamente.',
+      });
+    }
   }
 
   getRole(): void {
